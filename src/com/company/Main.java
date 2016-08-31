c3d6ac590cfe449f14c572f561fbf0352a0d166f
cameronkent <cameron.kent@my.jcu.edu.au>
2016-08-31 10:00:08 +1000

    29/8/16:     * Created some pseudocode for main     * Began coding of main and player.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+    Player userPlayer = new Player();
+    userPlayer.playerName();
+
+        System.out.println(userPlayer.playerName());
     }
+
 }
---
+++ src/com/company/Player.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by cameronkent on 31/08/2016.
+ */
+public class Player {
+
+    public String playerName() {
+        String name = JOptionPane.showInputDialog("Enter your name:");
+        return name;
+    }
+
+}
